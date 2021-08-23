1fa5507df64715e29c46bd9a58e9a35df3e68f9f
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-08-23 20:51:17 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 exports.min = function min (array) {
-  if (array.length === 0) return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
 
   array.sort((a, b) => a-b);
 
@@ -8,13 +8,13 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  if (array.length === 0) return 0;
+  if (!Array.isArray(array) || array.length === 0) return 0;
   array.sort((a, b) => b-a);
   return array[0];
 }
 
 exports.avg = function avg (array) {
-  // if(typeof array != "undefined" && arr != null && arr.length > 0){return 0};
+  if (!Array.isArray(array) || array.length === 0) return 0;
   let result = array.reduce((sum, item) => sum + item, 0);
 
   return result / (array.length);
