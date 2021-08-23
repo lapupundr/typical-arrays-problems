e3f37baff1981b283a7401668afdef75a15e500f
Tatsiana Zatonets <koshka.tan@gmail.com>
2021-08-23 17:47:27 +0300

    1 part ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array.length > 0) return 0;
+
+  array.sort((a, b) => a-b);
+
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array.length == 0) return 0;
+  array.sort((a, b) => b-a);
+  return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  // if(typeof array != "undefined" && arr != null && arr.length > 0){return 0};
+  let result = array.reduce((sum, item) => sum + item, 0);
+
+  return result / (array.length);
 }
